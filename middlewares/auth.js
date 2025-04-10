1c5947721156cdb2373584a53e9d8e467d5197d9
ConnectG_Solution <adityaakumar026@gmail.com>
2025-04-10 13:59:18 +0530

    Developed few APIs for Home-Screen

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,32 @@ import { User } from "../models/userSchema.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/error.js";
 
+export const isAdminAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(new ErrorHandler("User not authenticated!", 401));
+  }
+
+  const token = authHeader.split(" ")[1]; // ✅ Extract token after "Bearer "
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.id);
+   
+    if (!req.user) {
+      return next(new ErrorHandler("User not found!", 404));
+    }
+    if(req.user.role !="Admin"){
+      return next(new ErrorHandler("Not Authorized, You are not Admin!", 403));
+    }
+    next();
+  } catch (error) {
+    return next(new ErrorHandler("Invalid token!", 401));
+  }
+});
+
+
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
@@ -15,11 +41,11 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = await User.findById(decoded.id);
-
+   
     if (!req.user) {
       return next(new ErrorHandler("User not found!", 404));
     }
-
+    
     next();
   } catch (error) {
     return next(new ErrorHandler("Invalid token!", 401));
---
+++ models/categorySchema.js
@@ -0,0 +1,14 @@
+import mongoose, { mongo } from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  categoryName: {
+    type: String,
+    require: true,
+  },
+  categoryImage: {
+    public_id: String,
+    url: String,
+  },
+});
+
+export const Category = mongoose.model("Category", categorySchema);
