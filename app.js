380daedf03c061e224e399030a3f63847bd779b7
RamanKumar00 <ramankr7321@gmail.com>
2025-07-27 19:32:33 +0530

    Initial backend commit

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 import { errorMiddleware } from "./middlewares/error.js";
-import userRouter from "./routes/userRouter.js"
-import productRouter from "./routes/productRouter.js"
-import categoryRouter from "./routes/categoryRouter.js"
+import userRouter from "./routes/userRouter.js";
+import categoryRouter from "./routes/categoryRouter.js";
+import productRouter from "./routes/productRouter.js";
+
 const app = express();
-config({ path: "./config/config.env" });
+config({ path: "./config/config.env" }); // This will automatically look for .env file
 
-
-app.use(cors());
+app.use(
+  cors({
+    origin:"*",
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
@@ -27,8 +34,8 @@ app.use(
 
 //routes
 app.use("/api/v1/user", userRouter);
-app.use("/api/v1/product", productRouter);
 app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/product", productRouter);
 
 dbConnection();
 
